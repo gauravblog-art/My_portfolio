815d65808fe66ee608ada42e001dfcd7e4c51cb4
Gaurav kumar <76816526+gauravblog-art@users.noreply.github.com>
2022-11-20 12:04:12 +0530

    I have successfully loaded

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,6 @@ from portfolio import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 ]
