b1e169e2d87e962a8d6368c4c4cce34233e314f5
Gaurav kumar <76816526+gauravblog-art@users.noreply.github.com>
2022-12-03 23:13:22 +0530

    now i am going to deploye

--- portfolio_web/settings.py
+++ portfolio_web/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5nd-z6nti5si_^1#0fpe*m*78_+y^(s)14sw=dgv%dy9qgt!37'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 import os
 
 STATICFILES_DIRS=[
